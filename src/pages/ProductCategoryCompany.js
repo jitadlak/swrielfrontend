25b2dbb1281cb414886eb05c4baa592fcdbbe94c
jitadlak <87637308+jitadlak@users.noreply.github.com>
2023-09-19 10:35:20 +0530

    Update ProductCategoryCompany.js

--- src/pages/ProductCategoryCompany.js
+++ src/pages/ProductCategoryCompany.js
@@ -548,7 +548,7 @@ export default function ProductCategoryCompany() {
                                                       <img src={productImage} alt={productImage} style={{ height: 100, width: 100, margin: 20 }} />
                                                 </TableCell> */}
                         <TableCell align="left">{_id}</TableCell>
-                        <TableCell align="left">{productImage ?     <img src={productImage} alt={productImage} style={{ height: 100, width: 100, margin: 20 }} /> : {' not found'} }</TableCell>
+                        <TableCell align="left">{productImage ?     <img src={productImage} alt={productImage} style={{ height: 100, width: 100, margin: 20 }} /> : null }</TableCell>
                         <TableCell align="left">{productName}</TableCell>
 
                         <TableCell align="left">{productPrice} ₹</TableCell>
