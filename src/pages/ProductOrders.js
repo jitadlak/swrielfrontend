a62ef6d6824c7830666288b04027cdb1d3d5c497
jitadlak <87637308+jitadlak@users.noreply.github.com>
2023-11-27 16:01:49 +0530

    Update ProductOrders.js Reverse

--- src/pages/ProductOrders.js
+++ src/pages/ProductOrders.js
@@ -135,7 +135,7 @@ export default function ProductOrders() {
       if (res.data.status === 400) {
         alert(res.data.message);
       } else {
-        setUserList(res.data.result);
+        setUserList(res?.data?.result?.reverse();
       }
     } catch (error) {
       setLoading(false);
